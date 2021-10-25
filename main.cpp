b98422be090f763d38f3a9057ef42a487479e2c4
vickybot123 <hungviet.nguyen@hotmail.com>
2021-10-25 14:01:28 +0700

    fourth commit

--- main.cpp
+++ main.cpp
@@ -4,11 +4,23 @@ using namespace std;
 
 int main()
 {
-int len;
-cin >> len;
-int input[len];
-for(int i=0;i<len;i++)
-{
-
-}
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int len;
+    cin >> len;
+    int input[len];
+    for(int i=1; i<=len; i++)
+    {
+        cin >> input[i];
+    }
+    const int maxlen=1000;
+    int visited[maxlen]= {};
+    for(int i=1; i<=len; i++)
+    {
+        {
+            cin >> visited[i];
+            input[visited[i]]++;
+            if(input[visited[i]]==1) cout << visited[i] << " ";
+        }
+    }
 }
